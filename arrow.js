a5ee745826c177dbbbaf0bf4e5147893b1a424ac
Joao Borges <Joao.Borges@auster.local>
2024-08-27 17:49:27 -0300

    javascript atualizado

--- arrow.js
+++ arrow.js
@@ -2,10 +2,6 @@ window.addEventListener('scroll', function() {
     const arrow = document.querySelector('.arrow-down');
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const windowHeight = window.innerHeight;
-    
-    // Calcule a opacidade com base na rolagem
     const opacity = 1 - (scrollTop / windowHeight);
-
-    // Limita a opacidade para que nunca fique abaixo de 0
-    arrow.style.opacity = Math.max(opacity, 0);
+    arrow.style.opacity = Math.max(opacity, 0)
 });
